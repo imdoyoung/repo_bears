71c8f5766f5204bce8dec9275c0e5f1fec289670
imdoyoung <bear2008@naver.com>
2024-10-08 15:40:27 +0900

    USER - 로그인 Interceptor 완료

---
+++ bears/src/main/java/com/baseball/common/interceptor/CheckLoginSessionInterceptor.java
@@ -0,0 +1,31 @@
+package com.baseball.common.interceptor;
+
+import org.springframework.web.servlet.HandlerInterceptor;
+
+import com.baseball.infra.constants.Constants;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+// 인터페이스를 구현하는 클래스
+// HandlerInterceptor : 특정 URI 호출을 '가로채는' 역할, 이를 이용하여 기존 컨트롤러의 로직을 수정하기도 않고도, 사전이나 사후 제어가 가능
+public class CheckLoginSessionInterceptor implements HandlerInterceptor{
+	
+	// 로그인 여부를 체크해서 로그인이 되었을때만 페이지에 접근을 허용한다.
+	@Override
+	// HandlerInterceptor에서 제공하는 메서드
+	public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+		
+		System.out.println("로그인 확인, htmSeq : " + request.getSession().getAttribute("sessSeqXdm"));
+		
+		if(request.getSession().getAttribute("sessSeqXdm") != null) {	// 로그인이 되어 있을 때
+//			by pass
+		} else {
+			response.sendRedirect(Constants.URL_LOGINFORM);		// 로그인 페이지 경로로 이동
+			return false;	// 함수 탈출
+		}
+		
+		return HandlerInterceptor.super.preHandle(request, response, handler);
+	}
+
+}
--- bears/src/main/java/com/baseball/infra/constants/Constants.java
+++ bears/src/main/java/com/baseball/infra/constants/Constants.java
@@ -4,4 +4,5 @@ public class Constants {
 	public static final int ROW_NUM_TO_SHOW = 5;
 	public static final int PAGE_NUM_TO_SHOW = 5;
 	public static final int SESSION_MINUTE_XDM = 30;
+	public static final String URL_LOGINFORM = "/xdm/v1/infra/user/userXdmSignin";
 }
