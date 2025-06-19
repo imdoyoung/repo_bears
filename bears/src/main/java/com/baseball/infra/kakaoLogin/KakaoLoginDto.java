01d0a6d41ebdf5de36ffd586929fc4db48f12318
imdoyoung <bear2008@naver.com>
2025-06-19 21:53:52 +0900

    KakaoLogin_KakaoLoginDto 작성완료,,, service랑 controller는 아직...

---
+++ bears/src/main/java/com/baseball/infra/kakaoLogin/KakaoLoginDto.java
@@ -0,0 +1,36 @@
+package com.baseball.infra.kakaoLogin;
+
+public class KakaoLoginDto {
+	
+//	----- field -----
+	private String id;
+	private String nickname;
+	private String email;
+
+//	----- getter / setter -----
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public String getNickname() {
+		return nickname;
+	}
+	public void setNickname(String nickname) {
+		this.nickname = nickname;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	// 생성자
+	public KakaoLoginDto(String id, String nickname, String email) {
+		this.id = id;
+		this.nickname = nickname;
+		this.email = email;
+	}
+}
---
+++ bears/src/main/java/com/baseball/infra/kakaoLogin/KakaoLoginService.java
@@ -0,0 +1,7 @@
+package com.baseball.infra.kakaoLogin;
+
+public class KakaoLoginService {
+
+	// 카카오 로그인 후 사용자 정보를 가져옴
+	// 카카오 API를 통해 사용자 정보를 요청하고, 이를 KakaoUserDto 객체로 반환
+}
