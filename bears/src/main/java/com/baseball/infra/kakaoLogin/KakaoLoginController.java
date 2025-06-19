fde9bbc7071540bbe45fa4a6f3bfbeea480c8b72
imdoyoung <bear2008@naver.com>
2025-06-19 22:00:37 +0900

    카카오 로그인 수정

--- bears/src/main/java/com/baseball/infra/kakaoLogin/KakaoLoginController.java
+++ bears/src/main/java/com/baseball/infra/kakaoLogin/KakaoLoginController.java
@@ -7,6 +7,6 @@ public class KakaoLoginController {
 	// KakaoLoginService 에서 사용자 정보를 가져오고 KakaoLoginController 에서 이를 처리함
 	// 카카오 로그인 요청을 처리하고 콜백을 통해 사용자 정보를 받아 세션에 저장
 	
-//	@Autowired
-//	private KakaoLoginService kakaoLoginService;
+	//	@Autowired
+	//	private KakaoLoginService kakaoLoginService;
 }
